f132dc6c39a04c0dd160ea386dbff57ccdd1635f
Rivindu <rivinduprabha@gmail.com>
2023-11-29 10:55:06 +0530

    uploaded CSS and fxmls for events

--- src/main/java/com/sacms/sacmsood/MainApp.java
+++ src/main/java/com/sacms/sacmsood/MainApp.java
@@ -1,4 +1,34 @@
 package com.sacms.sacmsood;
 
-public class MainApp {
+import com.sacms.sacmsood.Models.User;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+    public static User user;
+
+    @Override
+    public void start(Stage stage) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("fxmls/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 840, 540);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setTitle("SACMS");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+    }
 }
